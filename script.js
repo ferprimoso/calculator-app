4188f8820e1059b25ae3fcdbd2547e2320136dfb
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2022-09-18 23:01:42 -0300

    Do main calculator logic

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
+    a = parseInt(a), b = parseInt(b);
     switch (operator) {
         case '+':
             return add(a, b); 
@@ -22,10 +23,10 @@ function operate(a, b, operator) {
         case '-':
             return subtract(a, b)
             break;
-        case '*':
+        case 'x':
             return multiply(a, b);
             break; 
-        case '/':
+        case '÷':
             return divide(a, b); ;       
             break;
 
@@ -42,29 +43,51 @@ const displayNum = document.querySelector('.result');
 const btnPressed = document.querySelectorAll('.btn');
 btnPressed.forEach(btn => btn.addEventListener('click', populateDisplay));
 
-let firstNum = 0;
-
-
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+let newOperation = false; 
 
 function populateDisplay(e) {
-
     const char = e.srcElement.outerText;
-    
-   if (!isNaN(parseInt(char))){
-        if (displayNum.textContent === '0' || isNaN(displayNum.textContent)) {
+
+    if (!isNaN(parseInt(char))){
+        if (displayNum.textContent === '0' || isNaN(displayNum.textContent)|| newOperation === true) {
             displayNum.textContent = e.srcElement.outerText;
+            newOperation = false;
         } else {
             displayNum.textContent += e.srcElement.outerText;
         }
 
     } else {
+        if (displayNum.textContent === '') return;
+
         if (char === 'C'){
-            displayNum.textContent = 0;
+            displayNum.textContent = '';
+            firstNum = null;
             return;
         }
-        let firstNum = displayNum.textContent;
-        let operator = char;
-        console.log(operator)
+        else if (char === '='){
+           if(firstNum == null) return;
+
+           secondNum = displayNum.textContent;
+           console.log(firstNum, secondNum, operator);
+           displayNum.textContent = operate(firstNum, secondNum, operator);
+           firstNum = displayNum.textContent;
+           secondNum = null;
+        }
+        else if (newOperation == false ) {
+            firstNum = displayNum.textContent;
+            operator = char;
+            displayNum.textContent = char;
+            console.log(operator)
+        } else {
+            secondNum = displayNum.textContent;
+            console.log(firstNum, secondNum, operator);
+            displayNum.textContent = operate(firstNum, secondNum, operator);
+            firstNum = displayNum.textContent;
+            newOperation = true;
+        }
     }
 };
 
